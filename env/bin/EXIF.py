0eb27b02333d232e3d039299ac7882ed0fb5f19b
root <root@nova>
2016-04-11 12:46:15 +0200

    add venv

---
+++ env/bin/EXIF.py
@@ -0,0 +1,4 @@
+#!/var/www/django/env/bin/python
+# EASY-INSTALL-SCRIPT: 'ExifRead==2.1.2','EXIF.py'
+__requires__ = 'ExifRead==2.1.2'
+__import__('pkg_resources').run_script('ExifRead==2.1.2', 'EXIF.py')
